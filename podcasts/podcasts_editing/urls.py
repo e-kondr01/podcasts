911119bd8074ed84b863353850d2e7ab6f0a35bf
e-kondr01 <e.kondr01@gmail.com>
2020-09-18 12:59:47 +0300

    fix

--- podcasts/podcasts_editing/urls.py
+++ podcasts/podcasts_editing/urls.py
@@ -4,7 +4,4 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('podcast', views.podcast, name='podcast'),
-    path('podcast/edit', views.edit, name='edits'),
-    path('photo/<str:name>', views.photo, name='photo'),
-    path('audio/<str:name>', views.audio, name='audio'),
 ]
